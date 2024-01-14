f3da6bd477d5d97421cfd9c92b06f8ad3194e8cc
Luc <luc.schrau@gmail.com>
2024-01-14 18:11:55 +0100

    Removed unfinished labextension

--- labextensions/sac_program_viewer/src/__tests__/sac_program_viewer.spec.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Example of [Jest](https://jestjs.io/docs/getting-started) unit tests
- */
-
-describe('sac_program_viewer', () => {
-  it('should be tested', () => {
-    expect(1 + 1).toEqual(2);
-  });
-});
